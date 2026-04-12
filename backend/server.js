bcd563a77d14ac9a50eb052393ef115135f3e1ae
Frontend-Joseph <jm2569@njit.edu>
2026-04-12 13:37:29 -0400

    Added Code to connect to MangoBD server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose'); /*MangoDB server*/
 const app = express();
-const port = 3000; 
+
+
+
+
+/* For the MangoDB server*/
+
+
+const DB_IP = 'ip add here '; 
+const DB_NAME = 'dp name';
+
+mongoose.connect(`mongodb://${DB_IP}:27017/${DB_NAME}`)
+  .then(() => console.log('Connected to Database VM successfully'))
+  .catch(err => console.error('Database connection failed:', err));
+
 
 app.get('/', (req, res) => { 
        res.send('Hello World from MEAN Stack!');
