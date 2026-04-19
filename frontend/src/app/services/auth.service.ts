c8c238dfed9cc17ed49bb78efe676ed50c3e9481
Joey Fox <jm2569@njit.edu>
2026-04-19 11:27:36 -0400

    Changed Login and Registering to match with server

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -3,31 +3,34 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   // Base URL of Joseph's backend server. Double check if it's the correct one!
-  private apiUrl = 'http://localhost:3000/api/auth';
+  private apiUrl = 'http://10.0.2.5:3000/auth'; //back end ip and port
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Sends a login request to the backend.
   login(username: string, password: string): Observable<any> {
     const body = {
-      username: username,
-      password: password,
-      'check/registering': 'check'
+      // to match with server {action, username, password}
+      action: 'login',
+      username,
+      password,
     };
     return this.http.post(this.apiUrl, body);
   }
 
   // Sends a register request to the backend.
-  register(username: string, password: string): Observable<any> {
+  register(username: string, email: string, dob: string, password: string): Observable<any> {
     const body = {
-      username: username,
-      password: password,
-      'check/registering': 'registering'
+      // to match with server {action, username, email, dob, password}
+      action: 'register',
+      username,
+      email,
+      dob,
+      password,
     };
     return this.http.post(this.apiUrl, body);
   }
